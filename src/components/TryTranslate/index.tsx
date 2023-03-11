5b759edba6ea8fe2b85e265bd8c275a87eb981bf
Admin <atapintolyan@yandex.ru>
2023-03-12 01:03:30 +0300

    update custom select

--- src/components/TryTranslate/index.tsx
+++ src/components/TryTranslate/index.tsx
@@ -1,10 +1,17 @@
-import {FC} from 'react'
+import {FC, useState} from 'react'
 import Input from "../../UI/Input";
 import ButtonRounded from "../../UI/ButtonRounded";
 import UploadIcon from "../../icons/UploadIcon";
 import Select from "../../UI/Select";
 import Radio from "../../UI/Radio";
 
+
+const options =[
+    "выбор 1",
+    "vibor 2",
+    "vibor 3"
+]
+const [selectedValue, setSelectedValue] = useState(options[0]);
 const TryTranslate: FC = () => {
 
     return (
@@ -14,7 +21,7 @@ const TryTranslate: FC = () => {
                     <h2 className="mb-14 font-bold text-4xl">Попробовать автоперевод</h2>
                     <div className="flex flex-col gap-y-14">
                         <Input placeholder="ссылка на изображение"/>
-                        <Select />
+                        <Select options={options} selectedValue={selectedValue} setSelectedValue={setSelectedValue}/>
                         <Radio />
                         <ButtonRounded classes="flex items-center gap-x-5 w-fit px-5 py-3 bg-black text-white">
                             <UploadIcon />
